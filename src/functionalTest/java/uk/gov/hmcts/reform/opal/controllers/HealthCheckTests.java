ae24d5e09ccedf3db7b4f6058632d38f1e742779
Sabah Irfan <sabah.irfan@gmail.com>
2024-02-12 17:29:10 +0000

    Add health check functional test

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/opal/controllers/HealthCheckTests.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.opal.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HealthCheckTests {
+
+    @LocalServerPort
+    private int port;
+
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
+
+    @Test
+    void healthCheckShouldReturnOk() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/health", String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode(), "Should return 200 ok");
+    }
+}
