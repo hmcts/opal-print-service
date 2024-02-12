9406d68ef673089a7e5b0a076265a1beffd2228d
Sabah Irfan <sabah.irfan@gmail.com>
2024-02-12 12:20:47 +0000

    Add Opal Print Service

--- src/main/java/uk/gov/hmcts/reform/opal/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/opal/controllers/RootController.java
@@ -23,6 +23,6 @@ public class RootController {
      */
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
-        return ok("Welcome to spring-boot-template");
+        return ok("Welcome to opal-print-service");
     }
 }
