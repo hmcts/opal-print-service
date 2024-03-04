23b6327f5b3d56072cab279c3f2fad5792b0ebf4
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-03-04 15:14:56 +0000

    Tidy-up and tests for printing dummy template

--- src/test/java/uk/gov/hmcts/opal/service/PrintServiceTest.java
+++ src/test/java/uk/gov/hmcts/opal/service/PrintServiceTest.java
@@ -8,6 +8,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import uk.gov.hmcts.opal.dto.PrintRequest;
+import uk.gov.hmcts.opal.dto.print.Address;
+import uk.gov.hmcts.opal.dto.print.DefendantAddress;
+import uk.gov.hmcts.opal.dto.print.Header;
+import uk.gov.hmcts.opal.dto.print.Offences;
+import uk.gov.hmcts.opal.dto.print.Schema;
 
 import java.io.ByteArrayOutputStream;
 import javax.xml.transform.Transformer;
@@ -39,8 +44,6 @@ class PrintServiceTest {
 
     @Test
     void testGeneratePdf() throws Exception {
-        // Mock input
-        PrintRequest printRequest = new PrintRequest();
 
         // Mock dependencies
         Transformer transformer = mock(Transformer.class);
@@ -49,7 +52,7 @@ class PrintServiceTest {
         when(fopFactory.newFop(eq(MimeConstants.MIME_PDF), any(), eq(pdfOutputStream))).thenReturn(null);
 
         // Call the method
-        byte[] result = printService.generatePdf(printRequest);
+        byte[] result = printService.generatePdf(buildPrintRequest());
 
         // Verify dependencies are called
         verify(transformerFactory).newTransformer(any(StreamSource.class));
@@ -57,5 +60,62 @@ class PrintServiceTest {
 
         // Assert the result
         assertNotNull(result);
+    }
+
+    private PrintRequest buildPrintRequest() {
+        return PrintRequest.builder()
+            .accountNumber("12345")
+            .caseNumber("2")
+            .amountOutstanding("100")
+            .dateOfOrder("10/12/2000")
+            .dateProduced("10/12/2000")
+            .defendantAddress(buildDefendantAddress())
+            .dob("10/12/2000")
+            .defendantInDefault("true")
+            .defendantName("John Smith")
+            .division("10")
+            .dwpapNumber("1")
+            .elapsedSecs("1000")
+            .endTime("1000")
+            .sex("M")
+            .header(buildHeader())
+            .offences(buildOffences())
+            .registerValidated("true")
+            .schema(buildSchema())
+            .sessionId("AA11")
+            .signature("Signature")
+            .build();
+    }
+
+    private DefendantAddress buildDefendantAddress() {
+        return DefendantAddress.builder()
+            .address(Address.builder()
+                         .line1("1 High Street")
+                         .postcode("W1 1AA")
+                         .build())
+            .name("John Smith")
+            .build();
+    }
+
+    private Header buildHeader() {
+        return Header.builder()
+            .code("code")
+            .line1("1 High Street")
+            .courtName("The court")
+            .build();
+    }
+
+    private Offences buildOffences() {
+        return Offences.builder()
+            .accountTotal("2000")
+            .totalImposition("10000")
+            .build();
+    }
+
+    private Schema buildSchema() {
+        return Schema.builder()
+            .met("22")
+            .libra("Schema")
+            .build();
     }
 }
