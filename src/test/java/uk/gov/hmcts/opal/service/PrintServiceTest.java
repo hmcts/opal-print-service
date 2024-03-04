4915b9fcf24ecd264b22ca20e65c9933019a7c77
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-03-04 15:33:51 +0000

    Checkstyle violation fix

--- src/test/java/uk/gov/hmcts/opal/service/PrintServiceTest.java
+++ src/test/java/uk/gov/hmcts/opal/service/PrintServiceTest.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class PrintServiceTest {
