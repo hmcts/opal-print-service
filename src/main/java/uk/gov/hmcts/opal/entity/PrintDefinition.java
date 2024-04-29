3094c550f274f81326ef7ea4054e9dd1d6462e22
TomReed <tom.reed@live.co.uk>
2024-04-29 09:41:26 +0100

    Add a simplified XSL to print db for test

--- src/main/java/uk/gov/hmcts/opal/entity/PrintDefinition.java
+++ src/main/java/uk/gov/hmcts/opal/entity/PrintDefinition.java
@@ -7,14 +7,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "print_definition")
