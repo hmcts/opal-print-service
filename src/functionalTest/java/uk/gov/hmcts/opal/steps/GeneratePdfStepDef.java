3cc13405fb517796dcee976aa26a8a09021e312c
TomReed <tom.reed@live.co.uk>
2024-05-08 09:16:32 +0100

    change functional print tests to in memory

--- src/functionalTest/java/uk/gov/hmcts/opal/steps/GeneratePdfStepDef.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/GeneratePdfStepDef.java
@@ -5,8 +5,7 @@ import io.restassured.response.Response;
 import net.serenitybdd.rest.SerenityRest;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -16,16 +15,8 @@ import java.util.logging.Logger;
 
 public class GeneratePdfStepDef extends BaseStepDef {
     static final Logger log = Logger.getLogger(GeneratePdfStepDef.class.getName());
-    ThreadLocal<String> pdfName = new ThreadLocal<>();
+    ThreadLocal<byte[]> pdfContent = new ThreadLocal<>();
 
-    private static void savePdfToFile(byte[] pdfContent, String filePath) {
-        log.info("Saving the PDF file: " + filePath);
-        try (FileOutputStream fos = new FileOutputStream(new File(filePath))) {
-            fos.write(pdfContent);
-        } catch (IOException e) {
-            log.info("Error occurred while saving PDF to file: " + e.getMessage());
-        }
-    }
 
     public static String dateTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyy-HHmmss");
@@ -44,16 +35,15 @@ public class GeneratePdfStepDef extends BaseStepDef {
             .body(jsonToPost)
             .when()
             .post(getTestUrl() + "/api/print/generate-pdf");
-        pdfName.set("ABD_" + dateTime() + "_.pdf");
-        savePdfToFile(response.getBody().asByteArray(), pdfName.get());
+        pdfContent.set(response.getBody().asByteArray());
     }
 
     @When("The pdf is syntactically correct")
     public void pdfSyntaxCorrect() throws IOException {
-        try (PDDocument pdf = PDDocument.load(new File(pdfName.get()))) {
-            log.info("Loaded PDF file + " + pdfName.get());
+        try (PDDocument pdf = PDDocument.load(new ByteArrayInputStream(pdfContent.get()))) {
+            log.info("Loaded PDF content from memory");
         } catch (IOException e) {
-            log.info("Error loading PDF file + " + pdfName.get());
+            log.info("Error loading PDF");
             log.info("PDF syntax is incorrect, could not be loaded: " + e);
             throw e;
         }
